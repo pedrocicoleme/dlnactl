c74604678ae730e30394d85757e11824fd6320ea
Pedro Leme <pedrocicoleme@gmail.com>
2018-03-07 00:03:55 -0300

    some more functions

--- dlnactl/main.py
+++ dlnactl/main.py
@@ -1,13 +1,20 @@
 import upnpclient
 
+
 fn = None
+
+
+AVTRANSPORT_SERVICE_ID = "urn:upnp-org:serviceId:AVTransport"
+
 
 class NotImplemented(Exception):
     def __init__(self,*args,**kwargs):
         Exception.__init__(self,*args,**kwargs)
 
+
 def raise_(exception):
     raise exception()
+
 
 AVAILABLE_COMMANDS = {
     "play": lambda device: device.AVTransport.Play(InstanceID=0, Speed="1"),
@@ -15,18 +22,26 @@ AVAILABLE_COMMANDS = {
     "stop": lambda device: device.AVTransport.Stop(InstanceID=0),
     "seek": lambda device, target: device.AVTransport.Seek(
         InstanceID=0, Unit="ABS_TIME", Target=target),
-    "next": lambda device: raise_(NotImplemented),
-    "previous": lambda device: raise_(NotImplemented),
+    "next_track": fn,
+    "previous_track": fn,
+    "next_media": lambda device: raise_(NotImplemented),
+    "previous_media": lambda device: raise_(NotImplemented),
     "set_media": fn,
     "set_next_media": fn,
     "get_status": fn,
-    "get_media_info": fn,
-    "set_mute": fn,
-    "set_volume": fn,
-    "get_mute": fn,
-    "get_volume": fn}
+    "get_media_info": lambda device: device.AVTransport.GetMediaInfo(
+        InstanceID=0),
+    "get_track_info": lambda device: device.AVTransport.GetPositionInfo(
+        InstanceID=0),
+    "set_mute": lambda device, mute: device.RenderingControl.SetMute(
+        InstanceID=0, Channel="Master", DesiredMute=mute),
+    "set_volume": lambda device, volume: device.RenderingControl.SetMute(
+        InstanceID=0, Channel="Master", DesiredVolume=volume),
+    "get_mute": lambda device: device.RenderingControl.GetMute(
+        InstanceID=0, Channel="Master"),
+    "get_volume": lambda device: device.RenderingControl.GetVolume(
+        InstanceID=0, Channel="Master")}
 
-AVTRANSPORT_SERVICE_ID = "urn:upnp-org:serviceId:AVTransport"
 
 def available_devices():
     devices = []
@@ -35,6 +50,7 @@ def available_devices():
         for service in device.services:
             if service.service_id == AVTRANSPORT_SERVICE_ID:
                 print(service.actions)
+
                 devices.append(device)
 
     return devices
@@ -55,4 +71,4 @@ if __name__ == "__main__":
         time.sleep(5)
         AVAILABLE_COMMANDS["play"](device)
 
-        AVAILABLE_COMMANDS["seek"](device, "0:01:30")
+        # AVAILABLE_COMMANDS["seek"](device, "0:01:30")
